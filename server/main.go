aee22eb4e61c3f32aeb6c88f89cb2b6a0fab9375
Ngô Mạnh Hùng <sundayy.dev@gmail.com>
2024-11-14 10:09:23 +0700

    create a connection to MongoDB

--- server/main.go
+++ server/main.go
@@ -2,19 +2,39 @@ package main
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
+	"log"
 	"net/http"
+	"os"
+	"server/components/appContext"
 	"server/middleware"
 	"server/routes"
 )
 
-func main(){
+func main() {
+
+	// load .env file
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("Set your 'MONGODB_URI' environment variable.")
+	}
+
+	appCtx := appContext.InitAppContext(uri)
+	defer appCtx.CloseDB()
+
 	v1 := http.NewServeMux()
 	v1.Handle("/v1/", http.StripPrefix("/v1", routes.UserRoutes()))
 
 	server := http.Server{
-		Addr: ":8080",
+		Addr:    ":8080",
 		Handler: middleware.Logging(v1),
 	}
 	fmt.Println("Server is running on port 8080")
-	server.ListenAndServe()
+
+	_ = server.ListenAndServe()
 }
